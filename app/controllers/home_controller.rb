c18a8dd3d3c58c968658cf044d1f2d1f060e0b43
Flavio-LP <flavio5070@outlook.com>
2025-05-27 21:04:07 -0300

    criados views do rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,11 @@ class HomeController < ApplicationController
    before_action :authenticate_user!
 
    def index
-      puts current_user.email
-      puts current_user.admin
       if current_user.admin?
-         
-         render 'adm'
+
+         render "adm"
       else
-         render 'index'
+         render "index"
       end
    end
-
 end
