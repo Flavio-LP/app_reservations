6c7d952180daae02bedeac105a0647dfd6dafe97
Flavio-LP <flavio5070@outlook.com>
2025-06-02 21:01:16 -0300

    ajustado alteração de senha customizada e página inicial do sistema

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,36 +1,18 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
-  # GET /resource/password/new
-   def edit
-      @user = User.find_by(email: params[:email])
-      @user.update(:password_digest => BCrypt::Password.create("abc123654"))
-      redirect_to new_user_session_path 
-   end
-
-  # POST /resource/password
-  # def create
-  #   super
-  # end
-
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
-
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def custom_reset
+    user = User.find_by(email: params[:email])
+    puts user
+    if user
+      user.password = "abc123654"
+      user.password_confirmation = "abc123654"
+      user.save(validate: false)
+      flash[:notice] = "Senha redefinida para abc123654"
+      redirect_to new_user_session_path
+    else
+      flash[:alert] = "E-mail não encontrado"
+      redirect_to new_user_password_path
+    end
+  end
 end
